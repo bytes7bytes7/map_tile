1c89883d95559935ef2f8175dee193bdded28f1c
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-07-10 18:24:28 +0300

    implement event handlers in TileBloc

--- lib/tile/tile_bloc.dart
+++ lib/tile/tile_bloc.dart
@@ -18,20 +18,102 @@ class TileBloc extends Bloc<TileEvent, TileState> {
   void _setLatitude(
     _SetLatitudeEvent event,
     Emitter<TileState> emit,
-  ) {}
+  ) {
+    if (event.latitude.isEmpty) {
+      return emit(
+        state.copyWith(
+          latitudeError: '',
+          latitude: null,
+        ),
+      );
+    }
+
+    final value = double.tryParse(event.latitude);
+
+    if (value == null) {
+      return emit(
+        state.copyWith(
+          latitudeError: 'Невалидное значение',
+        ),
+      );
+    }
+
+    emit(
+      state.copyWith(
+        latitudeError: '',
+        latitude: value,
+      ),
+    );
+  }
 
   void _setLongitude(
     _SetLongitudeEvent event,
     Emitter<TileState> emit,
-  ) {}
+  ) {
+    if (event.longitude.isEmpty) {
+      return emit(
+        state.copyWith(
+          longitudeError: '',
+          longitude: null,
+        ),
+      );
+    }
+
+    final value = double.tryParse(event.longitude);
+
+    if (value == null) {
+      return emit(
+        state.copyWith(
+          longitudeError: 'Невалидное значение',
+        ),
+      );
+    }
+
+    emit(
+      state.copyWith(
+        longitudeError: '',
+        longitude: value,
+      ),
+    );
+  }
 
   void _setZoom(
     _SetZoomEvent event,
     Emitter<TileState> emit,
-  ) {}
+  ) {
+    if (event.zoom.isEmpty) {
+      return emit(
+        state.copyWith(
+          zoomError: '',
+          zoom: null,
+        ),
+      );
+    }
+
+    final value = int.tryParse(event.zoom);
+
+    if (value == null) {
+      return emit(
+        state.copyWith(
+          zoomError: 'Невалидное значение',
+        ),
+      );
+    }
+
+    emit(
+      state.copyWith(
+        zoomError: '',
+        zoom: value,
+      ),
+    );
+  }
 
   void _submit(
     _SubmitEvent event,
     Emitter<TileState> emit,
-  ) {}
+  ) {
+    if (!state.canSubmit) {
+      return;
+    }
+  }
 }
