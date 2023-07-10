a702f58fa39cd54a9a11cd3fee2c585906c38afd
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-07-10 17:56:50 +0300

    add another formula

--- lib/parking_tile_calculator.dart
+++ lib/parking_tile_calculator.dart
@@ -7,14 +7,11 @@ const _tileSize = 256;
 class ParkingTileCalculator {
   const ParkingTileCalculator();
 
-  ParkingTileResult calculate({
+  ParkingTileResult microsoftCalculate({
     required double latitude,
     required double longitude,
     required int zoom,
   }) {
-    final mapWidth = _tileSize * pow(2, zoom);
-    final mapHeight = mapWidth;
-
     final sinLatitude = sin(latitude * pi / 180);
 
     final xPixel = ((longitude + 180) / 360) * _tileSize * pow(2, zoom);
@@ -27,5 +24,27 @@ class ParkingTileCalculator {
     final y = (yPixel / _tileSize).floor();
 
     return ParkingTileResult(x: x, y: y, z: zoom);
+  }
+
+  ParkingTileResult yandexCalculate({
+    required double latitude,
+    required double longitude,
+    required int zoom,
+  }) {
+    const e = 0.0818191908426;
+
+    final rho = pow(2, zoom + 8) / 2;
+    final beta = latitude * pi / 180;
+    final phi = (1 - e * sin(beta)) / (1 + e * sin(beta));
+    final theta = tan(pi / 4 + beta / 2) * pow(phi, e / 2);
+
+    final xp = rho * (1 + longitude / 180);
+    final yp = rho * (1 - log(theta) / pi);
+
+    return ParkingTileResult(
+      x: (xp / _tileSize).floor(),
+      y: (yp / _tileSize).floor(),
+      z: zoom,
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,22 @@
+import 'package:where_parking/parking_tile_calculator.dart';
 
+void main() {
+  const latitude = 55.757794;
+  const longitude = 37.599334;
+  const zoom = 18;
+
+  print(
+    ParkingTileCalculator().microsoftCalculate(
+      latitude: latitude,
+      longitude: longitude,
+      zoom: zoom,
+    ),
+  );
+  print(
+    ParkingTileCalculator().yandexCalculate(
+      latitude: latitude,
+      longitude: longitude,
+      zoom: zoom,
+    ),
+  );
+}
